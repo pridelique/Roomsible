930ce604e05bafd32efc96b4a36420605abca572
BelleChanantida <bellechanantida@gmail.com>
2025-06-10 11:41:27 +0700

    add params to schedule

--- app/schedule/page.jsx
+++ app/schedule/page.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import '../globals.css';
+import { useParams,useSearchParams } from "@node_modules/next/navigation"
+
 function Schedule() {
     const days=['วันจันทร์','วันอังคาร','วันพุธ','วันพฤหัสบดี','วันศุกร์'];
     const timeSlots=Array.from({length:9},(_,i)=> {
@@ -17,9 +19,14 @@ function Schedule() {
             time:`(${formatTime(startMinutes)}–${formatTime(endMinutes)})`,
         };
     });
+
+    const param=useParams()
+    const searchParams=useSearchParams();
+    const buildingId=Params.id
+    const roomNumber=searchParams.get('roomNumber')
     return (
     <div>
-        <h2 className="text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl font-semibold mb-10 mt-5">ตารางเวลา ตึกเอกอำนวยการ : ห้อง 1303</h2>    
+        <h2 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-semibold mb-10 mt-5">ตารางเวลา ตึกเอกอำนวยการ : ห้อง {roomNumber}</h2>    
         <div className="overflow-x-auto border border-gray-300 shadow-md rounded-md custom-scroll">
             <div
                 className="w-full border border-gray-300"
@@ -28,9 +35,9 @@ function Schedule() {
                     gridTemplateColumns:`100px repeat(${timeSlots.length},120px)`,
                     gridTemplateRows:`60px repeat(${days.length},60px)`,
             }}>
-                <div className="border border-gray-300 flex items-center justify-center font-bold bg-white">วัน/เวลา</div>
+                <div className="border border-gray-300 text-gray-700 flex items-center justify-center font-semibold bg-white">วัน/เวลา</div>
                 {timeSlots.map((period,index)=> (
-                    <div key={index} className="border border-gray-300 flex flex-col items-center justify-center font-bold bg-white text-sm px-1 text-center">
+                    <div key={index} className="border border-gray-300 flex flex-col items-center justify-center bg-white text-sm px-1 text-center">
                         <div className="text-gray-700">{period.label}</div>
                         <div className="text-xs text-gray-500">{period.time}</div>
                     </div>
@@ -38,7 +45,7 @@ function Schedule() {
 
                 {days.map((day)=> (
                     <React.Fragment key={day}>
-                        <div className="border border-gray-300 flex items-center justify-center font-semibold bg-white">{day}</div>
+                        <div className="border border-gray-300 flex items-center justify-center bg-white text-gray-700">{day}</div>
                         {timeSlots.map((_,index)=> (
                             <div key={`${day}-${index}`} className="border border-gray-200 bg-red-200 hover:bg-blue-50"></div>
                         ))}
