13fe2b31f1fdb4b57da0017e81a3d1255b4c2d4b
0PorpienG0 <porpieng2007@gmail.com>
2025-06-11 10:29:32 +0700

    add home page and modify some building

--- components/Building.jsx
+++ components/Building.jsx
@@ -1,207 +1,30 @@
-"use client";
+'use client';
 import buildings from "@data/buildings";
 import Room from "./Room";
-import React, { useEffect, useRef, useState } from "react";
-import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
-import { useRouter } from "@node_modules/next/navigation";
-import Image from "@node_modules/next/image";
-import { hand_zoom, zoom_in, zoom_out } from "@public/assets/icons";
-import { status } from "@data";
-import { set } from "mongoose";
-function Building({ id }) {
-  const router = useRouter();
-  const outerRef = useRef(null);
-  const innerRef = useRef(null);
-  const buttonRef = useRef(null);
-  const centerViewRef = useRef(null);
-  const [loading, setLoading] = useState(true);
-  const [zooming, setZooming] = useState(false);
-  const [fullscreen, setFullscreen] = useState(true);
-  const [scale, setScale] = useState(1);
-  const [maxScale, setMaxScale] = useState(1);
-  const [containerStyle, setcontainerStyle] = useState({
-    width: "100%",
-    justifyContent: "center",
-  });
+import React from "react";
 
-  const handleOnClick = (roomNumber) => {
-    router.push(`/building/${id}/schedule?roomNumber=${roomNumber}`);
-  };
-
-  const resetMessage = () => {
-    if (window.timeOutZooming) clearTimeout(window.timeOutZooming);
-    window.timeOutZooming = setTimeout(() => {
-      setZooming(false);
-    }, 5000);
-  };
-
-  const handleZoom = (centerView) => {
-    if (fullscreen) centerView(1);
-    else centerView(maxScale);
-    setZooming(true);
-    resetMessage()
-
-    setFullscreen(!fullscreen);
-  };
-
-  useEffect(() => {
-    const resize = () => {
-      const outer = outerRef.current;
-      const inner = innerRef.current;
-      if (!outer || !inner) return;
-
-      const scaleX = outer.clientWidth / inner.offsetWidth;
-      const scaleY = outer.clientHeight / (inner.offsetHeight * scaleX);
-
-      setScale(Math.min(1, scaleX));
-      setMaxScale(Math.max(1, scaleY));
-      if (Math.min(1, scaleX) === 1)
-        setcontainerStyle({
-          width: outer.clientWidth,
-          justifyContent: "center",
-        });
-      else
-        setcontainerStyle({
-          width: inner.offsetWidth * scaleX,
-          justifyContent: "start",
-        });
-    };
-    resize();
-    setTimeout(() => resize(), 10);
-    setLoading(false);
-    window.addEventListener("resize", resize);
-    return () => {
-      window.removeEventListener("resize", resize);
-    };
-  }, [id]);
-
-  useEffect(() => {
-    if (!loading && centerViewRef.current) {
-      centerViewRef.current(maxScale);
-    }
-  }, [scale, maxScale]);
-
+function Building({ id, handleOnClick, showName = true }) {
   return (
-    <>
-      {loading && (
-        <div className="w-full flex justify-center items-center">
-          <div className="border-6 border-gray-100 border-t-6 border-t-red-400 rounded-full p-6 animate-spin mt-20"></div>
-        </div>
-      )}
-      <div className={`${loading ? "opacity-0" : "opacity-100"}`}>
-        <TransformWrapper
-          panning={{
-            lockAxisY: true,
-          }}
-          maxScale={maxScale}
-          centerZoomedOut={true}
-          centerOnInit={true}
-          onZoom={(ref) => {
-            ref.centerView();
-            setZooming(true);
-            if (ref.state.scale === maxScale) setFullscreen(true);
-            if (ref.state.scale === 1) setFullscreen(false);
-          }}
-          onZoomStop={() => resetMessage()}
-          onPanning={() => setZooming(true)}
-          onPanningStop={() => resetMessage()}
-        >
-          {({ centerView }) => {
-            centerViewRef.current = centerView;
-            return (
-              <React.Fragment>
-                <h2 className="text-center text-2xl md:text-3xl lg:text-4xl text-gray-700 font-semibold">
-                  อาคาร {id} {buildings[id]["name"]}
-                </h2>
-                <p className="text-center text-slate-gray mt-2 text-sm md:text-base">
-                  เลือกห้องที่ว่างอยู่เพื่อทำการจอง
-                </p>
-                <div
-                  className="rounded-lg max-w-xl border border-gray-300 mx-auto relative mt-4"
-                  ref={outerRef}
-                >
-                  {!zooming && maxScale !== 1 && (
-                    <div className="absolute top-1/2 left-1/2 -translate-1/2 z-9 animate-pulse w-full text-center text-slate-gray text-lg sm:text-xl flex gap-2 justify-center items-center select-none">
-                      <Image
-                        src={hand_zoom}
-                        alt="hand zoom"
-                        width={24}
-                        height={24}
-                        draggable={false}
-                        className="select-none"
-                      />
-                      <p>ลากหรือใช้ปุ่มซูมเพื่อดูแผนผังอาคาร</p>
-                    </div>
-                  )}
-
-                  <TransformComponent>
-                    <div className="flex" style={containerStyle}>
-                      <div
-                        className="origin-left"
-                        ref={innerRef}
-                        style={{ transform: `scale(${scale})` }}
-                      >
-                        <div
-                          className="gap-1 grid p-4"
-                          style={{
-                            gridTemplateColumns: `repeat(${buildings[id]["col"]}, 80px)`,
-                            gridTemplateRows: `repeat(${buildings[id]["row"]}, 100px)`,
-                          }}
-                        >
-                          {buildings[id]["rooms"].map((row, rowIndex) => (
-                            <React.Fragment key={rowIndex}>
-                              {row.map((room, colIndex) => (
-                                <Room
-                                  key={`${rowIndex}-${colIndex}`}
-                                  {...room}
-                                  onClick={handleOnClick}
-                                />
-                              ))}
-                            </React.Fragment>
-                          ))}
-                        </div>
-                      </div>
-                    </div>
-                  </TransformComponent>
-                  <button
-                    className={`absolute bottom-0 right-0 rounded-full hover:bg-gray-200 opacity-40 p-2 m-2  object-cover cursor-pointer justify-center items-center ${
-                      maxScale === 1 ? "hidden" : "flex"
-                    } `}
-                    onClick={() => handleZoom(centerView)}
-                    ref={buttonRef}
-                  >
-                    <Image
-                      src={fullscreen ? zoom_in : zoom_out}
-                      alt="zoom"
-                      width={20}
-                      height={20}
-                      draggable={false}
-                      className="select-none"
-                    />
-                  </button>
-                </div>
-              </React.Fragment>
-            );
-          }}
-        </TransformWrapper>
-        <div className="flex justify-center max-w-xl mx-auto mt-4">
-          <div className="grid grid-cols-3 max-w-xl w-fit gap-x-2 sm:gap-x-3">
-            {status.map((item) => (
-              <div
-                className="flex gap-2 justify-start items-center text-slate-gray text-sm md:text-base"
-                key={item.statusEng}
-              >
-                <div
-                  className="size-3 sm:size-4 rounded-sm shadow-lg"
-                  style={{ backgroundColor: item.color }}
-                ></div>
-                <span>{item.statusThai}</span>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
-    </>
+    <div
+      className="gap-1.5 grid"
+      style={{
+        gridTemplateColumns: `repeat(${buildings[id]?.col}, 80px)`,
+        gridTemplateRows: `repeat(${buildings[id]?.row}, 100px)`,
+      }}
+    >
+      {buildings[id]?.rooms.map((row, rowIndex) => (
+        <React.Fragment key={rowIndex}>
+          {row.map((room, colIndex) => (
+            <Room
+              key={`${rowIndex}-${colIndex}`}
+              {...room}
+              handleOnClick={handleOnClick}
+              showName={showName}
+            />
+          ))}
+        </React.Fragment>
+      ))}
+    </div>
   );
 }
 
--- components/Room.jsx
+++ components/Room.jsx
@@ -1,6 +1,6 @@
 import { statusColors } from "@data"
 
-function Room({ roomNumber, name, status, col_span, onClick }) {
+function Room({ roomNumber, name, status, col_span, handleOnClick, showName }) {
   
   
   return (
@@ -8,13 +8,13 @@ function Room({ roomNumber, name, status, col_span, onClick }) {
         style={{ gridColumn: `span ${col_span} / span ${col_span}`,}}
         className='w-full'
     >
-        <div className={`relative z-8 bg-gray-200 rounded-md shadow-md flex justify-center items-center text-center text-sm w-full h-full
+        <div className={`relative z-1 bg-gray-200 rounded-md shadow-sm  flex justify-center items-center text-center text-sm w-full h-full p-1
         ${status === "none" ? 'hidden' : ''}
-        ${status === "available" ? 'cursor-pointer hover:scale-105' : ''}`}
+        ${status !== "none" && status !== "unavailable" ? 'cursor-pointer hover:scale-105' : ''}`}
           style={{ backgroundColor : statusColors[status]}}
-          onClick={status === 'available' ? () => onClick(roomNumber) : () => {}}
+          onClick={status !== "none" && status !== "unavailable" ? () => handleOnClick(roomNumber) : () => {}}
         >
-            {name}
+          {showName && name}
         </div>
         
     </div>
