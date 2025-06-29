609270cf85789d5d30d1967555b4a02527cccbaf
BelleChanantida <bellechanantida@gmail.com>
2025-06-29 22:30:18 +0700

    make login page responsive

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -42,87 +42,92 @@ function login() {
     }, 2000);
   };
   return (
-    <section className="padding-x max-container w-full pt-6">
-      {/* <ToastContainer/> */}
-      <div className="absolute top-1/2 left-1/2 -translate-1/2 w-full padding-x">
-        <div className="flex items-center justify-center text-center flex-col bg-white rounded-3xl shadow-md w-full max-w-[400px] mx-auto border border-gray-300 mb-7">
-          <div className="">
-            <Image
-              src={loginImgae}
-              alt="Login Image"
-              width={448}
-              height={300}
-              draggable="false"
-              className="rounded-t-3xl object-cover"
-            />
+  <section className="padding-x max-container w-full pt-6">
+    {/* <ToastContainer/> */}
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full padding-x">
+      <div className="flex flex-col sm:flex-row bg-white rounded-3xl shadow-md w-full max-w-[400px] sm:max-w-2xl mx-auto border border-gray-300 overflow-hidden mb-7">
+
+        {/* Image */}
+        <div className="flex-[1_1_200px] sm:w-[200px] h-[150px] sm:h-auto bg-gray-100">
+          <Image
+            src={loginImgae}
+            alt="Login Image"
+            width={448}
+            height={300}
+            draggable="false"
+            className="object-cover w-full h-full"
+          />
+        </div>
+
+        {/* Form */}
+        <div className="flex-[2_1_300px] px-7 pt-4 pb-8 w-full max-w-[325px] flex flex-col justify-center">
+          <div className="mt-1 text-[26px] font-semibold mb-4 text-gray-700 text-center sm:text-left">
+            เข้าสู่ระบบ
           </div>
-          <div className="px-7 pt-4 pb-8 w-full max-w-[325px]">
-            <div className="mt-1 text-[26px] font-semibold mb-4 text-gray-700">
-              เข้าสู่ระบบ
-            </div>
-            <form onSubmit={(e) => handleSubmit(e)}>
-              <ul className="flex flex-col gap-4">
-                <li className="flex items-center border-b border-gray-400 pb-2 ">
-                  <Mail className="w-5 h-5 text-gray-500 mr-3" />
-                  <input
-                    type="email"
-                    placeholder="Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    className="w-full outline-none placeholder-gray-500 text-gray-700"
-                    required
-                  />
-                </li>
-                <li className="flex items-center border-b border-gray-400 pb-2 w-full">
-                  <Lock className="w-5 h-5 text-gray-500 mr-3" />
-                  <input
-                    type={showPassword ? "text" : "password"}
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    className="w-full outline-none placeholder-gray-500 text-gray-700"
-                    required
-                  />
-                  <div className="mr-2 flex items-center justify-center">
-                    {showPassword ? (
-                      <Eye
-                        className="w-5 h-5 text-gray-500 cursor-pointer"
-                        onClick={() => setShowPassword(false)}
-                      />
-                    ) : (
-                      <EyeOff
-                        className="w-5 h-5 text-gray-500 cursor-pointer"
-                        onClick={() => setShowPassword(true)}
-                      />
-                    )}
-                  </div>
-                </li>
-              </ul>
-              {error && (
-                <div className="flex gap-2 justify-start items-center mt-3">
-                  <Warning className="w-5 h-5 text-red-500" />
-                  <p className="text-red-500 text-[12px] text-start whitespace-pre-line">
-                    {error}
-                  </p>
+          <form onSubmit={(e) => handleSubmit(e)}>
+            <ul className="flex flex-col gap-4">
+              <li className="flex items-center border-b border-gray-400 pb-2 ">
+                <Mail className="w-5 h-5 text-gray-500 mr-3" />
+                <input
+                  type="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="w-full outline-none placeholder-gray-500 text-gray-700"
+                  required
+                />
+              </li>
+              <li className="flex items-center border-b border-gray-400 pb-2 w-full">
+                <Lock className="w-5 h-5 text-gray-500 mr-3" />
+                <input
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="w-full outline-none placeholder-gray-500 text-gray-700"
+                  required
+                />
+                <div className="mr-2 flex items-center justify-center">
+                  {showPassword ? (
+                    <Eye
+                      className="w-5 h-5 text-gray-500 cursor-pointer"
+                      onClick={() => setShowPassword(false)}
+                    />
+                  ) : (
+                    <EyeOff
+                      className="w-5 h-5 text-gray-500 cursor-pointer"
+                      onClick={() => setShowPassword(true)}
+                    />
+                  )}
                 </div>
+              </li>
+            </ul>
+            {error && (
+              <div className="flex gap-2 justify-start items-center mt-3">
+                <Warning className="w-5 h-5 text-red-500" />
+                <p className="text-red-500 text-[12px] text-start whitespace-pre-line">
+                  {error}
+                </p>
+              </div>
+            )}
+            <button
+              type="submit"
+              disabled={loading}
+              className="mt-5 w-full py-2 text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:bg-gradient-to-br focus:outline-none shadow-red-500/50 font-medium text-[15px] text-center rounded-xl shadow-md cursor-pointer flex items-center justify-center transition-all duration-200 ease-in-out disabled:cursor-auto"
+            >
+              {loading ? (
+                <div className="border-2 border-t-2 border-transparent border-t-white animate-spin size-[22px] rounded-full"></div>
+              ) : (
+                <p>{buttonText}</p>
               )}
-              <button
-                type="submit"
-                disabled={loading}
-                className="mt-5 w-full py-2 text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:bg-gradient-to-br focus:outline-none shadow-red-500/50 font-medium text-[15px] text-center rounded-xl shadow-md cursor-pointer flex items-center justify-center transition-all duration-200 ease-in-out disabled:cursor-auto"
-              >
-                {loading ? (
-                  <div className="border-2 border-t-2  border-transparent border-t-white animate-spin size-[22px] rounded-full"></div>
-                ) : (
-                  <p>{buttonText}</p>
-                )}
-              </button>
-            </form>
-          </div>
+            </button>
+          </form>
         </div>
       </div>
-    </section>
-  );
+    </div>
+  </section>
+);
+
 }
 
 export default login;
