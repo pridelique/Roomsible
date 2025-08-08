f823927b54a5ed254e1e0e8ac6a1a71418644c33
0PorpienG0 <porpieng2007@gmail.com>
2025-08-08 15:32:27 +0700

    modify prorfile image size

--- components/nav_components/Profile.jsx
+++ components/nav_components/Profile.jsx
@@ -24,13 +24,13 @@ function Profile({ handleLogout }) {
         className="flex items-center justify-center cursor-pointer opacity-50 hover:opacity-80 focus:opacity-80  transition-all duration-200"
         onClick={() => setTimeout(() => setProfileDropdown(!profileDropdown), 0)}
       >
-        <Guest_Profile className="size-10 text-slate-gray opacity-50" />
+        <Guest_Profile className="size-11 text-slate-gray opacity-50" />
       </button>
       {profileDropdown && (
-        <div className="absolute w-40 right-0 top-full bg-white shadow-md rounded-lg z-10 text-sm overflow-hidden border border-gray-300 text-slate-gray" ref={dropdownRef}>
+        <div className="absolute w-42 right-0 top-full bg-white shadow-md text-[16px] rounded-lg z-10 overflow-hidden border border-gray-300 text-slate-gray" ref={dropdownRef}>
           <ul>
             <li className="hover:bg-gray-100">
-              <button className="w-full px-4 py-2 cursor-pointer" onClick={handleLogout}>
+              <button className="w-full px-3 py-[6px] cursor-pointer" onClick={handleLogout}>
                 ออกจากระบบ
               </button>
             </li>
