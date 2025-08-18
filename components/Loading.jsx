0b2657937a1d1cf67a3ac271a768d7738e3e2e32
0PorpienG0 <porpieng2007@gmail.com>
2025-08-18 13:29:32 +0700

    modified loading ui

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -1,7 +1,11 @@
+import "@styles/loading.css"
+
 function Loading() {
   return (
-    <div className="fixed inset-0 bg-white bg-opacity-75 flex justify-center items-center z-50">
-      <div className="border-3 border-gray-100 border-t-3 border-t-red-400 rounded-full p-6 animate-spin shadow-inner"></div>
+    <div className="fixed inset-0 bg-[#f5f5f5] bg-opacity-75 flex justify-center items-center z-50 scale-120">
+      {/* <div className="border-3 border-gray-100 border-t-3 border-t-red-400 rounded-full p-6 animate-spin shadow-inner"></div> */}
+      <div className="pin"></div>
+      <div className="pulse"></div>
     </div>
   );
 }
